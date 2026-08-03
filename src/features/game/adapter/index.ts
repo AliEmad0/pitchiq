a2476ed72fdc964adf1d46f639eee2abd215b0ce
Ali Emad <aliemademad02@gmail.com>
2026-08-03 19:47:59 +0300

    feat(game): GameTeam aggregate + domain/adapter barrels (TASK-1801)

---
+++ src/features/game/adapter/index.ts
@@ -0,0 +1,4 @@
+import "server-only";
+export * from "./formation";
+export * from "./player";
+export * from "./squad";
---
+++ src/features/game/domain/index.ts
@@ -0,0 +1,6 @@
+export * from "./card-id";
+export * from "./eligibility";
+export * from "./formation";
+export * from "./player";
+export * from "./ratings";
+export * from "./team";
